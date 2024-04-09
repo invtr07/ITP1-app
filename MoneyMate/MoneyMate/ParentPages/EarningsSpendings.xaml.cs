570d58799dcf8747b0c5f494c860a0bb4087cc61
invtr07 <95737990+invtr07@users.noreply.github.com>
2024-04-09 13:44:15 +0100

    added troubled doughtnut chart into cashflow page

--- MoneyMate/MoneyMate/ParentPages/EarningsSpendings.xaml.cs
+++ MoneyMate/MoneyMate/ParentPages/EarningsSpendings.xaml.cs
@@ -8,24 +8,34 @@ namespace MoneyMate.ParentPages
 	public partial class EarningsSpendings : ContentPage
 	{
         public Transactions[] transactions;
+        public Transactions[] doughNutCategories;
 
         public EarningsSpendings ()
 		{
 			InitializeComponent ();
 
             transactions = new Transactions[]{
-                new Transactions{ TransactionID="123", Treference="Lloydsbank", DT= new DateTime(2024, 4, 6, 10, 30, 0),Category="Monthly fees" , Amount= 500.00m},
-                new Transactions{ TransactionID="123", Treference="Lloydsbank", DT= new DateTime(2024, 4, 6, 10, 30, 0),Category="Monthly fees", Amount= 500.00m},
-                new Transactions{ TransactionID="123", Treference="Lloydsbank", DT= new DateTime(2024, 4, 6, 10, 30, 0), Category="Monthly fees", Amount= 500.00m},
-                new Transactions{ TransactionID="123", Treference="Lloydsbank", DT= new DateTime(2024, 4, 6, 10, 30, 0), Category="Monthly fees", Amount= 500.00m},
+                new Transactions{ TransactionID="123", Treference="Lloydsbank", DT= new DateTime(2024, 4, 6, 10, 30, 0),Category="A" , Amount= 500.00m},
+                new Transactions{ TransactionID="123", Treference="Lloydsbank", DT= new DateTime(2024, 4, 6, 10, 30, 0),Category="B", Amount= 500.00m},
+                new Transactions{ TransactionID="123", Treference="Lloydsbank", DT= new DateTime(2024, 4, 6, 10, 30, 0), Category="C", Amount= 500.00m},
+                new Transactions{ TransactionID="123", Treference="Lloydsbank", DT= new DateTime(2024, 4, 6, 10, 30, 0), Category="D", Amount= 500.00m},
 
-                new Transactions{ TransactionID="123", Treference="Lloydsbank", DT= new DateTime(2024, 4, 6, 10, 30, 0), Category="Monthly fees", Amount= 500.00m},
-                new Transactions{ TransactionID="123", Treference="Lloydsbank", DT= new DateTime(2024, 4, 6, 10, 30, 0), Category="Monthly fees", Amount= 500.00m},
-                new Transactions{ TransactionID="123", Treference="Lloydsbank", DT= new DateTime(2024, 4, 6, 10, 30, 0), Category="Monthly fees", Amount= 500.00m}
+            
+
+            };
+            doughNutCategories = new Transactions[] {
+                new Transactions { Category="A", Amount= 500.00m},
+                new Transactions { Category = "B", Amount = 300.00m },
+                new Transactions { Category="C", Amount= 700.00m}
+
 
             };
 
+
+
             ListViewT.ItemsSource = transactions;
+            doughNut.ItemsSource = transactions;
+
         }
 
         void SeeAllTapped(System.Object sender, System.EventArgs e)
@@ -41,6 +51,7 @@ namespace MoneyMate.ParentPages
             public string Category { get; set; }
             public decimal Amount { get; set; }
         }
+
 
     }
 }
